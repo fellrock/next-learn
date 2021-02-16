a4cb84f58a2bea13ae8795b9d6438e04d8645506
Felipe Moreira <felipe.cardoso.rj@gmail.com>
2021-02-16 19:57:11 -0300

    variavel de ambiente

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -14,7 +14,3 @@ async function tempo(request, response) {
 
 export default tempo;
 
-
-//https://api.convertkit.com/v3/subscribers?api_secret=ZJwLOhiO1EvB90Clpsyw1e0fGRzPfw1Q
-//https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UC8Jd-aVn0IsYLtVyABvWmVA&key=AIzaSyAnmDCkM-vYLmLlbsT1nTEu-amTtMynqR0
-//items.statistics.subscriberCount
