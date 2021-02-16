73ad6d4a158bcad099b539b99cc1bd1353642e35
Felipe Moreira <felipe.cardoso.rj@gmail.com>
2021-02-16 16:54:54 -0300

    Updating final changes

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,17 +1,18 @@
 function Tempo(props){
+    console.log{'> Passando pelo Frontend;'}
+    const dynamicDate = new Date();
+    const dynamicDateString = dynamicDate.toGMTString();
 
-        const dynamicDate = new Date();
-        const dynamicDateString = dynamicDate.toGMTString();
-
-        return(
-            <div>
-                <div>{dynamicDateString} (dinâmico)</div>
-                <div>{props.staticDateString} (estático)</div>
-            </div>)
+    return(
+        <div>
+            <div>{dynamicDateString} (dinâmico)</div>
+            <div>{props.staticDateString} (estático)</div>
+        </div>)
 
 }
 
 export function getStaticProps(){
+    console.log{'> Passando pelo getStaticProps();'}
     const staticDate = new Date();
     const staticDateString = staticDate.toGMTString();
 
